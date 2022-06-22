dfd6b07b1b365dca7baccbd6b799f88a8cd2906a
cfmbido <cfmbido@gmail.com>
2022-06-22 11:07:53 +0700

    add fetch data SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { GetServerSidePropsContext, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
+  const { data } = props;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,12 +15,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{data.name}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +59,24 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await result.json();
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  };
 }
 
-export default Home
+export default Home;
